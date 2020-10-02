f40b6944c999ccda0905a9c83358cf2b1a9107c7
Damian de Vega <damiandevega@gmail.com>
2020-10-02 10:15:46 -0400

    making icon path absolute

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const createMainWindow = () => {
     title: 'ImageShrink',
     width: 500,
     height: 600,
-    icon: './assets/icons/Icon_256x256.png',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
   });
 
   mainWindow.loadFile('./app/index.html');
