c6c427ea1ee80fe24bec324125f0599959d02b14
Damian de Vega <damiandevega@gmail.com>
2020-10-06 19:44:35 -0400

    adding global shortcuts for reloading and dev tools

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 
 // set environment
 process.env.NODE_ENV = 'development';
@@ -15,19 +15,11 @@ const createMainWindow = () => {
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
     resizable: isDev,
+    backgroundColor: 'white',
   });
 
   mainWindow.loadFile('./app/index.html');
 };
-
-app.on('ready', () => {
-  createMainWindow();
-
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
-
-  mainWindow.on('closed', () => (mainWindow = null));
-});
 
 const menu = [
   ...(isMac ? [{ role: 'appMenu' }] : []),
@@ -36,11 +28,26 @@ const menu = [
     submenu: [
       {
         label: 'Quit',
+        accelerator: 'CmdOrCtrl+W',
         click: () => app.quit(),
       },
     ],
   },
 ];
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
+    mainWindow.toggleDevTools()
+  );
+
+  mainWindow.on('closed', () => (mainWindow = null));
+});
 
 app.on('window-all-closed', () => {
   if (!isMac) {
