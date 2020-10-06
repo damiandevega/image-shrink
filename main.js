8204cbb0de14d59c29c555e5a9c0d72bb4d1bea8
Damian de Vega <damiandevega@gmail.com>
2020-10-06 09:25:51 -0400

    adding some configruations for mac and an isDev environment variable

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 
+// set environment
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
 let mainWindow;
 
 const createMainWindow = () => {
@@ -8,9 +14,22 @@ const createMainWindow = () => {
     width: 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
   });
 
   mainWindow.loadFile('./app/index.html');
 };
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
